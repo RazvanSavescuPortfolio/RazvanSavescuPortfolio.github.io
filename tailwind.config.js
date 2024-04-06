3c4ca2d96377e1f394d9f6a6629fc663d1c5573b
Razvan <razvan.sav00@gmail.com>
2024-04-06 19:42:58 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         from: "rgba(39,39,50,0.8)",
         to: "rgba(66,28,50,0.8)",
         fnav: "rgba(20,20,60,0.8)",
-        tnav: "rgba(35,35,50,0.8)"
+        tnav: "rgba(35,35,50,0.8)",
     }
     },
     fontFamily:{
