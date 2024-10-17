a497654a1a38f32a9ce296254c7c2de6c170f4f9
Razvan <razvan.sav00@gmail.com>
2024-10-17 11:30:49 +0300

    navbar shrink

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ menubtn.addEventListener("click", ()=>{
     menu.classList.toggle("hidden")
     menu.classList.toggle("")
 })
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    var element = document.querySelector("nav");
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    element.style.padding = "8px";
+  } else {
+    element.style.padding = "12px";
+  }
+} 
